edea5dbb8bf514c97777612d83141f3188e9c9f8
precious28 <31155317+precious28@users.noreply.github.com>
2021-09-14 20:21:10 +0200

    i added date of birth. adddress area and category to choose from doctor and patient

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -18,18 +18,29 @@ const Register = () => {
                 <p className='signup-contents'>Create your account to get started</p>
                 
                 <form action="#" className='signup-form' onSubmit >
+
+               <div>
+               <label for="">Register as:</label>
+              <select name="category" id="category"> 
+                 <option value="choose on">Choose one</option>
+                   <option value="patient">Patient</option>
+                   <option value="doctor">Doctor</option>
+                   </select>
+               </div>
+               <br />
+
                     <div>   
                     <input type="text" id="fullname" name="firstname" placeholder='Full Name'/> 
                     </div>
-
+                         <input type="date" id="birthday" name="birthday" placeholder= 'Date of Birth'/>
                    <div>
-                    <input type="email" name='email' id='email' placeholder='Email'/>
+                    <input type="email" name='email' id='email' placeholder='Email'/>  
                    </div>
 
                     <div>
                     <input type="password" name='password' id='password' placeholder='Password'/>
                     </div>
-                
+             <textarea rows="4" cols="40" name="comment" form="usrform">Enter your Address here...</textarea>               
                     <button className= 'signup-btn' type='submit'>Sign Up</button>
                 </form>
 
