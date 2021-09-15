9d152ecfa0a42d9018db51d539f28558193ae9d3
yvehogan <mz.yve3@gmail.com>
2021-09-15 03:36:51 -1100

    updated landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import Dashboard from "./layouts/dashboard";
+// import Dashboard from "./layouts/dashboard";
+
+
+// function App() {
+//   return (
+//     <div>
+//       <p>
+//         <Dashboard />
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+
 
 
 function App() {
   return (
-    <div>
-      <p>
-        <Dashboard />
-      </p>
+    <div className="App">
+
+    <BrowserRouter>	
+    <Switch>
+      <Route exact path='/Homepage'>
+
+        <Homepage />
+      </Route>   
+       
+    <Route exact path="/" component={Homepage}>
+      </Route>
+
+    </Switch>
+		</BrowserRouter>
     </div>
   );
 }
